6cb737a05051de2dbf63027ce75509044fdd776b
TusharThanvi1990 <143414158+TusharThanvi1990@users.noreply.github.com>
2023-10-26 16:08:25 +0530

    Update Youtube-downloader.py

--- Youtube-downloader.py
+++ Youtube-downloader.py
@@ -5,16 +5,16 @@ def download_video(link,directory):
     print("To see all strem options press 1 \n to see stram option only in mp4 format press 2 \n for ONLY audio options press 3\n for ONLY video options press 4")
     command = int(input())
     if command == 1:
-        print("Bellow are the all stream option to download (it may take few seconds )\n")
+        print("Below are the all stream option to download (it may take few seconds )\n")
         print(ytobj.streams.filter())
     elif command == 2:
-        print("Bellow are the stream option in mp4 format to download (it may take few seconds )\n")
+        print("Below are the stream option in mp4 format to download (it may take few seconds )\n")
         print(ytobj.streams.filter(file_extension = 'mp4'))
     elif command == 3:
-        print("Bellow are the stream option in only audio format to download (it may take few seconds )\n")
+        print("Below are the stream option in only audio format to download (it may take few seconds )\n")
         print(ytobj.streams.filter(only_audio=True))
     elif command == 4:
-        print("Bellow are the stream option in only video format to download (it may take few seconds )\n")
+        print("Below are the stream option in only video format to download (it may take few seconds )\n")
         print(ytobj.streams.filter(only_video =True))
     print("\nEnter the itag of the stream you want to download\n")
     itag = int(input())
